000cda1512287e8bd521297dad10d23e1bc1869d
鵜巷 <u@chimata.org>
2018-11-07 12:17:22 +0900

    Initial Commit

---
+++ Akanas/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Akanas
+//
+//  Created by Private on 2018/11/07.
+//  Copyright © 2018 鵜巷. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
